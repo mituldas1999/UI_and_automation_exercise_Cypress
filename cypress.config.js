0a4dc890074eace8c4135739f11c6b152faf6125
Mitul Das <mituldas751@gmail.com>
2024-12-13 22:37:28 +0600

    Report the API TEST

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,12 @@ module.exports = defineConfig({
     },
     specPattern: "cypress/e2e/**/*.cy.js", // Include all test files in the `e2e` folder
     baseUrl: "https://automationexercise.com", // Set a base URL for API requests
+    reporter: "mocha", // Set Mocha as the reporter
+    reporterOptions: {
+      reportDir: "cypress/results", // Directory to save the report
+      overwrite: false,
+      html: true,  // Generate HTML report
+      json: true   // Generate JSON report
+    }
   },
 });
